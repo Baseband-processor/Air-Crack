bc7da3a3c5faf3cb487c85d080a8dfe3bc65ed2e
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-10 08:40:07 +0200

    Update Crack.pm

--- perl/lib/Air/Crack.pm
+++ perl/lib/Air/Crack.pm
@@ -6,6 +6,24 @@ require  v5.22.1;
 use strict;
 use warnings;
 
+use constant PACKET_302 => "HTTP/1.1 302 Found\r\n\
+Location: https://www.google.com/?gws_rd=ssl\r\n\
+Cache-Control: private\r\n\
+Content-Type: text/html; charset=UTF-8\r\n\
+Date: Sun, 30 Nov 2014 03:25:47 GMT\r\n\
+Server: gws\r\n\
+Content-Length: 231\r\n\
+X-XSS-Protection: 1; mode=block\r\n\
+X-Frame-Options: SAMEORIGIN\r\n\
+Alternate-Protocol: 80:quic,p=0.02\r\n\
+\r\n\
+<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\n\
+<TITLE>302 Moved</TITLE></HEAD><BODY>\n\
+<H1>302 Moved</H1>\n\
+The document has moved\n\
+<A HREF=\"https://www.google.com/?gws_rd=ssl\">here</A>.\r\n\
+</BODY></HTML>\r\n";
+
 our $VERSION = '0.03';
 use base qw(Exporter DynaLoader);
 
